fe457111928aed415b16b23706d70733d137016f
vincent <a.mlynarski@exlabs.pl>
2017-09-20 12:12:58 +0200

    Navigation left menu base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { NavBarComponent } from './navigation/nav-bar/nav-bar.component';
 import { NavMenuComponent } from './navigation/nav-menu/nav-menu.component';
 import { NavigationService } from './navigation/navigation.service';
+import { NavMenuLeftComponent } from './navigation/nav-menu/nav-menu-left/nav-menu-left.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     NavBarComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    NavMenuLeftComponent
   ],
   imports: [
     BrowserModule,
