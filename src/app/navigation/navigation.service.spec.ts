c711bb0ae0c4b3fd6c4e5ef9e2453830f2ba0eea
vincent <a.mlynarski@exlabs.pl>
2017-09-21 09:09:18 +0200

    Removed unnecessary tests

--- src/app/navigation/navigation.service.spec.ts
+++ src/app/navigation/navigation.service.spec.ts
@@ -1,15 +1,22 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { HttpModule } from '@angular/http';
 
 import { NavigationService } from './navigation.service';
+
 
 describe('NavigationService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpModule],
       providers: [NavigationService]
     });
   });
 
-  it('should be created', inject([NavigationService], (service: NavigationService) => {
-    expect(service).toBeTruthy();
+  it('should return proper icon class', inject([NavigationService], (service: NavigationService) => {
+    const iconName = 'someIcon.svg';
+    const iconWihoutSvg = 'someIcon';
+    const iconPrefix = 'gl-icon-';
+    const iconClass = service.getElementIconClass(iconName);
+    expect(iconClass).toBe(iconPrefix + iconWihoutSvg);
   }));
 });
